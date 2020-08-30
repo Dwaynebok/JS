07a1356dacaa7d3db2341e49f9cc5687ce6d5550
Dwaynebok <dwaynebok19@gmail.com>
2020-08-30 21:14:29 +0200

    retried maths

---
+++ 02-maths/03-even-square/script.js
@@ -0,0 +1,28 @@
+/* becode/javascript
+ *
+ * /02-maths/03-even-square/script.js - 2.3: carrés des pairs
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    document.getElementById("run").addEventListener("click", function() {
+
+    let a = 1;
+    let b = 21;
+    let acc = [];
+    for (i = a; i <= b; i++) {
+        if (Math.sqrt(i) == parseInt(Math.sqrt(i)))
+        {acc.push(i)};
+    }
+
+    alert(acc);
+
+    });
+
+})();
---
+++ 02-maths/06-fizzbuzz/script.js
@@ -0,0 +1,23 @@
+/* becode/javascript
+ *
+ * /02-maths/06-fizzbuzz/script.js - 2.6: fizzbuzz
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+    console.log(
+        Array.apply(null, {length: 100}).map(function(val, index) {
+            index++;
+            if (index % 15 == 0){return "FizzBuzz";}
+            if (index % 3 == 0){return "Fizz";}
+            if (index % 5 == 0){return "Buzz";}
+            return index;
+        }).join('\n')
+    );
+
+})();
