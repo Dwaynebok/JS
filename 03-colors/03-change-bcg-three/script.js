3e6e99b83358cac3a7267d8258c980af2b1033cd
Dwaynebok <dwaynebok19@gmail.com>
2020-08-31 10:32:00 +0200

    understanding colors

---
+++ 03-colors/03-change-bcg-three/script.js
@@ -0,0 +1,36 @@
+/* becode/javascript
+ *
+ * /03-colors/03-change-bcg-three/script.js - 3.3: couleur de fond (3)
+ *
+ * coded by leny@BeCode
+ * started at 26/10/2018
+ */
+
+// NOTE: don't focus on the existing code structure for now.
+// You will have time to focus on it later.
+
+(function() {
+
+    const body = document.querySelector("body");
+    const button = document.querySelector("#run");
+
+
+
+    const generateColor = function (){
+
+        let letters = "0123456789ABCDEF";
+        let color = "#"
+
+        for (let i = 0; i < 6 ;i++){
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+
+    }
+
+        button.addEventListener("click",function (){
+            body.style.background = generateColor();
+        });
+
+
+})();
